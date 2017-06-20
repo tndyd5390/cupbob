2cf3f185fb25a84c3409020a2722bda8aab7b922
tndyd <tndyd5390@naver.com>
2017-06-20 19:07:25 +0900

    Develope start

--- src/com/cupbob/controller/admin/AdminBoardController.java
+++ src/com/cupbob/controller/admin/AdminBoardController.java
@@ -1,16 +1,16 @@
-package com.cupbob.controller.user;
+package com.cupbob.controller.admin;
 
 import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 
-import com.cupbob.service.ICommentService;
+import com.cupbob.service.IBoardService;
 
 @Controller
-public class CommnetController {
+public class AdminBoardController {
 	private Logger log = Logger.getLogger(this.getClass());
 	
-	@Resource(name="CommentService")
-	private ICommentService commentService;
+	@Resource(name="BoardService")
+	private IBoardService boardService;
 }
--- src/com/cupbob/controller/admin/AdminMenuController.java
+++ src/com/cupbob/controller/admin/AdminMenuController.java
@@ -1,16 +1,16 @@
-package com.cupbob.controller.user;
+package com.cupbob.controller.admin;
 
 import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 
-import com.cupbob.service.ICommentService;
+import com.cupbob.service.IMenuService;
 
 @Controller
-public class CommnetController {
+public class AdminMenuController {
 	private Logger log = Logger.getLogger(this.getClass());
 	
-	@Resource(name="CommentService")
-	private ICommentService commentService;
+	@Resource(name="MenuService")
+	private IMenuService menuService;
 }
