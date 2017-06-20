4506d98c6d647fa26c60ad2348fb593e976a1c50
moon <hoop_ster@naver.com>
2017-06-20 19:15:12 +0900

    1st cording test

--- src/com/cupbob/controller/user/MenuController.java
+++ src/com/cupbob/controller/user/MenuController.java
@@ -1,15 +1,38 @@
 package com.cupbob.controller.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import com.cupbob.service.IMenuService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cupbob.service.IUserService;
 
 
 @Controller
 public class MenuController {
 	private Logger log = Logger.getLogger(this.getClass());
-	
-	@Resource(name="MenuService")
-	private IMenuService menuService;
+	@Resource(name="userService")
+	private IUserService userService;
+
+	@RequestMapping(value="userList")
+	public String getUserList(HttpServletRequest req, HttpServletResponse resp, Model model) throws Exception{
+		
+		log.info("getUserList start");
+		
+		List userList = userService.getUserList();
+		if(userList == null){
+			userList = new ArrayList<>();
+		}
+		model.addAttribute("userList", userList);
+		log.info("getUserList end");
+		return "admin/adminUserJoin";
+		
+	}
 }
