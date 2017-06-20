d9a9c749b1ecfa41cbe285c744a6163934928a50
choihanyong <rambo0228@naver.com>
2017-06-20 19:20:46 +0900

    sdsd

---
+++ src/com/cupbob/controller/admin/AdminMenuController.java
@@ -0,0 +1,31 @@
+package com.cupbob.controller.admin;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cupbob.service.IMenuService;
+
+@Controller
+public class AdminMenuController {
+	private Logger log = Logger.getLogger(this.getClass());
+	
+	@Resource(name="MenuService")
+	private IMenuService menuService;
+
+	@RequestMapping(value="menuList")
+	public String getUserList(HttpServletRequest req, HttpServletResponse resp, Model model) throws Exception{
+		
+		log.info("menuList start");
+		
+		System.out.println("test");
+		
+		log.info("menuList end");
+		return "admin/adminMenuList";
+	}
+}
