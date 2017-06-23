0ca9c0f9b4422650fe233ee21f86f29db1aa1684
tndyd <tndyd5390@naver.com>
2017-06-23 12:50:32 +0900

    adminmenu except modify other complete

--- src/com/cupbob/service/IMenuService.java
+++ src/com/cupbob/service/IMenuService.java
@@ -6,4 +6,7 @@ import com.cupbob.dto.Product_infoDTO;
 
 public interface IMenuService {
 	public List<Product_infoDTO> getAdminMenuList() throws Exception;
+	public int insertAdminMenu(Product_infoDTO pDTO) throws Exception;
+	public Product_infoDTO getAdminMenuDetail(Product_infoDTO pDTO) throws Exception;
+	public int deleteAdminMenuOne(Product_infoDTO pDTO) throws Exception;
 }
