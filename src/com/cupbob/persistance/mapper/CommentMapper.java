9d4d1bd478c30ab93f6313e90576862a7a2aec70
choihanyong <rambo0228@naver.com>
2017-06-23 10:32:03 +0900

    frdsafa

--- src/com/cupbob/persistance/mapper/CommentMapper.java
+++ src/com/cupbob/persistance/mapper/CommentMapper.java
@@ -1,8 +1,11 @@
 package com.cupbob.persistance.mapper;
 
 import com.cupbob.config.Mapper;
+import com.cupbob.dto.Comment_infoDTO;
 
 @Mapper("CommentMapper")
 public interface CommentMapper {
+
+	void createComment(Comment_infoDTO cDTO) throws Exception;
 
 }
--- src/com/cupbob/service/impl/CommentService.java
+++ src/com/cupbob/service/impl/CommentService.java
@@ -1,9 +1,21 @@
 package com.cupbob.service.impl;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
+
+import com.cupbob.dto.Comment_infoDTO;
+import com.cupbob.persistance.mapper.CommentMapper;
 import com.cupbob.service.ICommentService;
 
 @Service("CommentService")
-public class CommnetService implements ICommentService{
+public class CommentService implements ICommentService{
 
+	@Resource(name="CommentMapper")
+	private CommentMapper commentMapper;
+	
+	@Override
+	public void createComment(Comment_infoDTO cDTO) throws Exception {
+		commentMapper.createComment(cDTO);
+	}
 }
