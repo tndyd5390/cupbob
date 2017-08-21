4407dd67d0663d801a65e02ad05cbc10b26d6421
moon <hoop_ster@naver.com>
2017-08-21 21:01:41 +0900

    t

--- src/com/cupbob/service/impl/ChartService.java
+++ src/com/cupbob/service/impl/ChartService.java
@@ -1,9 +1,11 @@
 package com.cupbob.service.impl;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.annotation.Resource;
 
@@ -12,7 +14,6 @@ import org.springframework.stereotype.Service;
 import com.cupbob.dto.Chart_DTO;
 import com.cupbob.persistance.mapper.ChartMapper;
 import com.cupbob.service.IChartService;
-import com.cupbob.util.CmmUtil;
 
 @Service("ChartService")
 public class ChartService implements IChartService {
@@ -37,7 +38,41 @@ public class ChartService implements IChartService {
 	@Override
 	public List<Chart_DTO> getWeekGender() throws Exception {
 		List<Chart_DTO> chList = chartMapper.getWeekGender();
+		HashMap<String, String> rMap = new HashMap<String, String>();
+		
+		for(Chart_DTO cDTO : chList){
+			if(rMap.containsKey(cDTO.getReg_dt())){
+				String e[] = rMap.get(cDTO.getReg_dt()).split(",");
+				int e1 = Integer.parseInt(e[0])+Integer.parseInt(cDTO.getMale_count()); 
+				int e2 = Integer.parseInt(e[1])+Integer.parseInt(cDTO.getFemale_count());
+				
+				rMap.put(cDTO.getReg_dt(), String.valueOf(e1)+","+String.valueOf(e2));
+				
+			}else{
+				rMap.put(cDTO.getReg_dt(), cDTO.getMale_count()+","+cDTO.getFemale_count());
+			}
+			
+		}
+		chList.clear();
+		
+		for(String key : rMap.keySet()){
+			Chart_DTO ctDTO = new Chart_DTO();
+			ctDTO.setReg_dt(key);
+			String male = rMap.get(key).toString().split(",")[0];
+			String fmale =  rMap.get(key).toString().split(",")[1];
+			ctDTO.setMale_count(male);
+			ctDTO.setFemale_count(fmale);
+			chList.add(ctDTO);
+			System.out.println(ctDTO.getReg_dt());
+			System.out.println(ctDTO.getMale_count());
+			System.out.println(ctDTO.getFemale_count());
+			
+		}
+		
+		
+		/*
 		Chart_DTO reDTO = new Chart_DTO();
+		
 		for(int i=0; i<chList.size();i++){
 			if(i>1){
 				if(chList.get(i).getReg_dt().equals(chList.get(i-1).getReg_dt())){
@@ -48,10 +83,16 @@ public class ChartService implements IChartService {
 					reDTO.setMale_count(String.valueOf(malecount));
 					reDTO.setFemale_count(String.valueOf(femalecount));
 					
-					chList.add(reDTO);
+					sDTO[i] = reDTO;
+				}else{
+					reDTO.setReg_dt(chList.get(i).getReg_dt());
+					reDTO.setMale_count(chList.get(i).getMale_count());
+					reDTO.setFemale_count(chList.get(i).getMale_count());
+					sDTO[i] = reDTO;
 				}
 			}
 		};
+		*/
 		return chList;
 	}
 }
