4f0d83080cc577bb2dacc25d95bf9d5552d37b01
oohs00oohs <oohs00oohs@gmail.com>
2018-12-04 14:00:14 +0900

    배치 프로그램 완료

--- src/com/cupbob/crontab/Scheduler.java
+++ src/com/cupbob/crontab/Scheduler.java
@@ -28,7 +28,7 @@ public class Scheduler {
 	@Resource(name="OrderService")
 	private IOrderService orderService;
 	
-	@Scheduled(cron = "0 0 02 * * *") 
+	@Scheduled(cron = "0 57 13 * * *") 
 	public void cronTest1() throws Exception{ 
 		String[] prdt_no = {"17","18","19","20","21","22","23","24","25","26"};
 		String[] prdt_price = {"3000","3000","3000","2500","3000","2000","2000","1500","1500","3000"};
@@ -43,12 +43,12 @@ public class Scheduler {
 			int randomNo = CmmUtil.randomRange(0, 9);
 			int qty = CmmUtil.randomRange(1, 20);
 			int rec_yn = CmmUtil.randomRange(0, 1);
-			int price = Integer.parseInt(prdt_price[randomNo]) * qty;
+			//int price = Integer.parseInt(prdt_price[randomNo]) * qty;
 			ordNo = (Long.parseLong(ordNo) + 1) + "";
 			Order_infoDTO oDTO = new Order_infoDTO();
 			oDTO.setOrd_no(ordNo);
-			oDTO.setTotal_ord_price(price + "");
-			oDTO.setReal_ord_price(price + "");
+			//oDTO.setTotal_ord_price(price + "");
+			//oDTO.setReal_ord_price(price + "");
 			oDTO.setPayment_tp("P");
 			oDTO.setUsr_rcv_time("12:00");
 			oDTO.setRcv_yn(yn[rec_yn]);
@@ -57,18 +57,24 @@ public class Scheduler {
 			
 			Set<String> set = new HashSet<String>();
 			int tmp = CmmUtil.randomRange(1, 9);
+			int price = 0;
 			for(int j = 0;j< tmp; j++){
-				set.add(prdt_no[CmmUtil.randomRange(0, 9)]);
+				int prdtNo = CmmUtil.randomRange(0, 9);
+				set.add(prdt_no[prdtNo]);
+				price += Integer.parseInt(prdt_price[prdtNo]) * qty;
 			}
 			
 			List<Order_itemDTO> oList = new ArrayList<Order_itemDTO>();
-			for(String prdtNo : set){				
+			for(String prdtNo : set){				   
 				Order_itemDTO oiDTO = new Order_itemDTO();
 				oiDTO.setPrdt_no(prdtNo);
 				oiDTO.setOrd_amnt(qty + "");
 				oiDTO.setReg_user_no(userNo[rec_yn]);
 				oList.add(oiDTO);
 			}
+			
+			oDTO.setReal_ord_price(price + "");
+			oDTO.setTotal_ord_price(price + "");
 			result += orderService.insertDummyData(oDTO, oList);
 		}
 	} 
